2a8d1c2c4a048221c16386b6277bfd8a3437d16c
moh4wk <cristiano.desimone@gmail.com>
2023-08-24 18:01:51 +0200

    test-changes

--- vuln_scanner.py
+++ vuln_scanner.py
@@ -7,9 +7,8 @@ import scanner
 
 c = Console()
 
-# target_url = "http://192.168.3.139/"
-target_url = "https://sivanandamusic.it"
-# target_url = "http://192.168.3.139/mutillidae/"
+target_url = "http://192.168.3.139/"
+target_url = "http://192.168.3.139/mutillidae/"
 # target_url = "http://192.168.3.139/dvwa/"
 # login_url = "http://192.168.3.139/dvwa/login.php"
 # links_to_ignore = ["http://192.168.3.139/dvwa/logout.php"]
